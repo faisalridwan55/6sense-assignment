13f4f8150489366dad24a4acd8172a2d5b0b9434
Faisal Ridwan <faisal.intern@stoqo.com>
2022-07-24 13:56:57 +0700

    Finish API call

--- src/containers/Teams/index.js
+++ src/containers/Teams/index.js
@@ -2,23 +2,32 @@ import { useEffect, useState } from "react";
 import { api } from "../../api";
 
 const Teams = () => {
-    const [data, setData] = useState({})
-    const [isLoading, setLoading] = useState(false)
-    
-    useEffect(() => {
-        setLoading(true)
-        const fetchData = async () => {
-            const resp = await api.get('/getTeams')
-            console.log(resp);
-            setData(resp)
-        }
-        fetchData()
-        setLoading(false)
-    }, [])
+  const [teams, setTeams] = useState([]);
+  const [activities, setActivities] = useState([]);
+  const [currentUser, setCurrentUser] = useState({});
+  const [isLoading, setLoading] = useState(false);
 
-    return (
-        <div>TEST</div>
-    )
-}
+  useEffect(() => {
+    setLoading(true);
+    const fetchData = async () => {
+      const {
+        data: { teams, activities, current_user },
+      } = await api.get("/getData");
+
+      setTeams(teams);
+      setActivities(activities);
+      setCurrentUser(current_user);
+    };
+
+    fetchData();
+    setLoading(false);
+  }, []);
+
+  console.log(teams);
+  console.log(activities);
+  console.log(currentUser);
+
+  return <div>TEST</div>;
+};
 
 export default Teams;
